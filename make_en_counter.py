f4733858e22ef90af4552f74eebab9d4d70bded1
john lee <inno314159@gmail.com>
2021-05-22 00:37:45 +0900

    push test

--- make_en_counter.py
+++ make_en_counter.py
@@ -18,8 +18,8 @@ p_list = [p1,q1,q2,q3,q4]
 en_chars = []
 ko_words = []
 
-path = "/home/john/Notebook/"
-path2 = "preProject/create_vocabs_en/data/"
+path = "" #/home/john/Notebook/"
+path2 = "out/" #preProject/create_vocabs_en/data/"
 
 def modify_sent(sent,p_list):
     for p in p_list:
@@ -39,35 +39,35 @@ def count_en():
     
     print('start reading wiki_files')
     en_wk = Counter()
-    for ik in tqdm(range(1,28)):
-        with open(path+"wiki_en/wiki_iter"+str(ik)+".txt",'r') as f:
+    for ik in tqdm(range(160)):
+        with open(path+"wiE/wiki_en"+str(ik)+".txt",'r') as f:
             X = f.read().split('\n')    
         en_wk = word_count(X, en_wk)     
     print("len(en_wk) :{}".format(len(en_wk)))
-    json_save(en_wk,path+path2+'counted_vocs_en_wk_check')
+    json_save(en_wk,path+path2+'counted_vocs_en_wk')
 
     print('start reading en_news_files')
     en_news = Counter()
     for i in tqdm(range(1,4)):
-        df = pd.read_csv(path+'news_Data/articles'+str(i)+'.csv') 
+        df = pd.read_csv(path+'newsEn/articles'+str(i)+'.csv') 
         en_news = word_count(df['content'], en_news)
     print("len(en_news) :{}".format(len(en_news)))
-    json_save(en_news,path+path2+'counted_vocs_en_news_check')
+    json_save(en_news,path+path2+'counted_vocs_en_news')
 
     print('start reading translated_ko_news_files')
     en_count = Counter()
     for i in tqdm(range(1,5)):
-        df = pd.read_excel(path+'Data/3_문어체_뉴스('+str(i)+')_191213.xlsx')
+        df = pd.read_excel(path+'newsKo/3_문어체_뉴스('+str(i)+')_191213.xlsx')
         parallel = df['ID 원문 번역문'.split(' ')]
         en_count = word_count(parallel['번역문'], en_count)
 
     print("len(en_count) :{}".format(len(en_count)))
-    en_count = Counter({k:v//3 for k,v in en_count.items()})  
+    #en_count = Counter({k:v//3 for k,v in en_count.items()})  
 
     print("len(en_count) :{}".format(len(en_count)))    
-    json_save(en_count,path+path2+'counted_vocs_ko_news_check')
+    json_save(en_count,path+path2+'counted_vocs_ko_news')
 
-    json_save(en_count+en_news+en_wk,path+path2+'counted_vocs_check')
+    json_save(en_count+en_news+en_wk,path+path2+'counted_vocs')
 
 if __name__ == '__main__':
     count_en()
